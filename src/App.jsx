b80d2681a37ae4f95b2c7b9edbcc7daa4b4f892c
GagandeepSingh0115 <gs816912@gmail.com>
2021-08-18 12:55:27 +0530

    feat:tailwind+react+vite

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return <h1 className="text-5xl">Hello vite</h1>;
+};
+
+export default App;
